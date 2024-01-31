2435d143d1d13a802182edb7d3cf68ae1fed1a35
Yousef <yousefjavaherian@gmail.com>
2024-01-31 05:04:05 +0330

    Quartz sync: Jan 31, 2024, 5:04 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,13 +3,13 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🌌Riding the Milky Way",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "blog.yousefjavaherian.ir",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
